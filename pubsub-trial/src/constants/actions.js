c6af6790cc93fb44cb8939a179576584e0a37848
Nora Leonte <nora.leonte@evozon.com>
2022-01-29 12:39:38 +0200

    basic implementation of a state manager with pubsub

--- pubsub-trial/src/constants/actions.js
+++
@@ -1,9 +0,0 @@
-const COUNTER = {
-  increment: (state) => {
-    return { counter: state + 1 };
-  },
-  decrement: (state) => {
-    return { counter: state - 1 };
-  },
-};
-export default COUNTER;
